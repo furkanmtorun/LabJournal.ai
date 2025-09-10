5e25ac7477f60ebd64a0ab9c1d2f04cdc267a159
Furkan Torun <furkanmtorun@gmail.com>
2025-09-11 00:11:32 +0200

    add api and http file

--- api/main.py
+++ api/main.py
@@ -1,9 +1,14 @@
 """API module via FastAPI."""
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from botocore.exceptions import ClientError
 from pydantic import BaseModel
 
+import boto3
 _VERSION: str = "0.1.0"
 _TITLE: str = "LabJournal.AI - API"
+_TABLE_NAME :str = "experiments"
+_REGION_NAME: str = "eu-central-1"
+DB_CLIENT = boto3.client("dynamodb", region_name=_REGION_NAME)
 
 # Models
 class VersionModel(BaseModel):
@@ -16,7 +21,6 @@ class HealthCheckModel(BaseModel):
 class ExperimentBaseModel(BaseModel):
     name: str
     category: str
-    image: str
     timestamp: str
     status: str
 
@@ -38,4 +42,69 @@ def root_version() -> VersionModel:
 
 @app.get("/health", response_model=HealthCheckModel, description="Health check")
 def health_check() -> HealthCheckModel:
-    return HealthCheckModel(status="OK")
+    """Health check."""
+    response = DB_CLIENT.describe_table(TableName=_TABLE_NAME)["Table"]["TableStatus"]
+    if response == "ACTIVE":
+        return {"status": "OK"}
+    else:
+        return {"status": f"Error: {response}"}
+
+@app.get("/experiments", response_model=list[ExperimentModel])
+async def get_experiments() -> list[ExperimentModel]:
+    """Returns all experiments."""
+    response = DB_CLIENT.scan(TableName=_TABLE_NAME)["Items"]
+    experiments_db: list[ExperimentModel] = []
+    for item in response:
+        experiment_data = {
+            "id": item["id"]["S"],
+            "name": item["name"]["S"],
+            "category": item["category"]["S"],
+            "timestamp": item["timestamp"]["S"],
+            "status": item["status"]["S"],
+            "result": item["result"]["S"],
+        }
+        experiments_db.append(ExperimentModel(**experiment_data))
+    return experiments_db
+
+@app.get("/experiments/{experiment_id}", response_model=ExperimentModel)
+async def get_project(experiment_id: str) -> ExperimentModel:
+    """Returns the project for the provided experiment ID."""
+    response = DB_CLIENT.get_item(
+        TableName=_TABLE_NAME,
+        Key={
+            "id": {"S": experiment_id}  # DynamoDB column is 'id'
+        }
+    )
+    item = response.get("Item")
+    if item:
+        experiment_data = {
+            "id": item["id"]["S"],
+            "name": item["name"]["S"],
+            "category": item["category"]["S"],
+            "timestamp": item["timestamp"]["S"],
+            "status": item["status"]["S"],
+            "result": item["result"]["S"]
+        }
+        return ExperimentModel(**experiment_data)
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Error: Experiment with the ID of {experiment_id} not found!"
+    )
+    
+@app.delete("/experiments/{experiment_id}")
+async def delete_experiment(experiment_id: str):
+    try:
+        response = DB_CLIENT.delete_item(
+            TableName=_TABLE_NAME,
+            Key={
+                'id': {'S': experiment_id}
+            },
+            ConditionExpression="attribute_exists(id)"  # Ensures item exists
+        )
+        return {"message": f"Experiment {experiment_id} deleted successfully."}
+    except ClientError as e:
+        if e.response['Error']['Code'] == "ConditionalCheckFailedException":
+            raise HTTPException(status_code=404, detail=f"Experiment {experiment_id} not found.")
+        else:
+            raise HTTPException(status_code=500, detail=str(e))
